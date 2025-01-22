b59c193e67480d6911682b368e40fc718e8d4b03
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-01-21 22:29:19 -0500

    refactor GitHub Actions workflow and simplify backend configuration in backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,3 @@
 terraform {
-  backend "s3" {
-    bucket         = "dubovsky-andrey-terraform-stage-bucket"
-    key            = "aws-notification-service/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-    encrypt        = true
-  }
+  backend "s3" {}  
 }
