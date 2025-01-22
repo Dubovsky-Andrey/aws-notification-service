f0f806abd48422c43af9281cade5c04c5f5fd237
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-01-22 17:31:46 -0500

    refactor IAM role module for Lambda integration by correcting variable assignments and improving naming consistency

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-module "iam_role" {
+module "iam-role-for-lambda"" {
   source = "./modules/iam-role-for-lambda"
 
-  role_name             = var.lambda_role_name"LambdaRole"
-  role_path             = var.role_path"/"
-  assume_role_services  = var.assume_role_services ["lambda.amazonaws.com"]
+  role_name             = var.lambda_role_name
+  role_path             = var.role_path
+  assume_role_services  = var.assume_role_services
   enable_cloudwatchlogs = true
   enable_sns_and_ses    = true
 }
