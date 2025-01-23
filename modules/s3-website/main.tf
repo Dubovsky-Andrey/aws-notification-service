f1596b0a7e0006a98d251d0d6efbd937abd65f11
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-01-23 15:43:24 -0500

    update S3 bucket public access settings: allow public ACLs and policies

--- modules/s3-website/main.tf
+++ modules/s3-website/main.tf
@@ -10,9 +10,9 @@ resource "aws_s3_bucket" "static_site" {
 resource "aws_s3_bucket_public_access_block" "static_site_public_access_block" {
   bucket = aws_s3_bucket.static_site.id
 
-  block_public_acls       = true
+  block_public_acls       = false
   block_public_policy     = false
-  ignore_public_acls      = true
+  ignore_public_acls      = false
   restrict_public_buckets = false
 }
 
