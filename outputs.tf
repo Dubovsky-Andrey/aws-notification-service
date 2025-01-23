e895e976278d6185c1165d9c34cc278dd3ac4058
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-01-22 20:08:32 -0500

    update outputs.tf to reference new IAM role module output variables for role ID and ARN

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "role_id" {
   description = "The ID of the IAM role"
-  value       = module.iam-role-for-lambda.aws_iam_role.this.id
+  value       = module.iam-role-for-lambda.role_id
 }
 
 output "role_arn" {
   description = "The ARN of the IAM role"
-  value       = module.iam-role-for-lambda.aws_iam_role.this.arn
+  value       = module.iam-role-for-lambda.role_arn
 }
