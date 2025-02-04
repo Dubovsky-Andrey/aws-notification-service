b0598975a829af3f5c2f0703f48833bc9ce9e3dd
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-02-04 16:33:08 -0500

    comment out output variables for API Gateway and S3 website

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
-output "api_gateway_endpoint" {
-  description = "API Gateway endpoint URL"
-  value       = module.api_gateway.api_endpoint
-}
+# output "api_gateway_endpoint" {
+#   description = "API Gateway endpoint URL"
+#   value       = module.api_gateway.api_endpoint
+# }
 
-output "s3_website_url" {
-  description = "S3 Website URL"
-  value       = module.s3_website.website_url
-}
+# output "s3_website_url" {
+#   description = "S3 Website URL"
+#   value       = module.s3_website.website_url
+# }
