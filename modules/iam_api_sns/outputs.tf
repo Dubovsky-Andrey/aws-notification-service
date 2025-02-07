0244de13d6d338c20db8abe2137764d48ffb3858
Dubovsky-Andrey <120334571+Dubovsky-Andrey@users.noreply.github.com>
2025-02-06 20:51:26 -0500

    refactor iam_api_sns: rename output to apigateway_sns_role_arn for clarity

--- modules/iam_api_sns/outputs.tf
+++ modules/iam_api_sns/outputs.tf
@@ -1,7 +1,7 @@
 /*
 Outputs for IAM module.
 */
-output "lambda_role_arn" {
-  description = "The ARN of the Lambda execution role"
+output "apigateway_sns_role_arn" {
+  description = "The ARN of the AIA gateway execution role"
   value       = aws_iam_role.apigateway_sns_role.arn
 }
